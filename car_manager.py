036f0592102dad7f709a5c9bb6e58bdf0496fd68
Akshat Girdhar <84849106+coderakg@users.noreply.github.com>
2022-12-04 17:48:51 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -13,19 +13,17 @@ class CarManager:
         self.car_speed = STARTING_MOVE_DISTANCE
         
     def create_car(self): # a function that will create cars
-        # to reduce the numbers of cars formed we will do this:
         random_chance = random.randint(1,6)
-        if random_chance == 1 : # now this will decrease the number of cars created
-            new_car = Turtle("square") # the new car will be an object of the Turtle class 
+        if random_chance == 1 : 
+            new_car = Turtle("square")  
             new_car.shapesize(stretch_wid=1,stretch_len=2)
-            new_car.penup() # to avoid the turtle drawing 
+            new_car.penup()  
             new_car.color(random.choice(COLORS))
             random_y = random.randint(-220,220)
             new_car.goto(300,random_y)
-            self.all_cars.append(new_car) # the new car object will be appended to the all cars list
+            self.all_cars.append(new_car) 
 
     def move_cars(self):
-        # to make the cars move accross the screen
         for car in self.all_cars:
             car.backward(self.car_speed)
             
